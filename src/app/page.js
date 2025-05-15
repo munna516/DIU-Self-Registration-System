acf9fd70e036748e650650d88c26e144cda648ca
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2025-05-15 10:33:05 +0600

    added login and registration form

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className="text-3xl text-blue-800 text-center mt-20">
-        Diu Self Registration System
-      </h1>
-    </>
-  );
-}
